a5fec9b95fb0e6d83c57d28029e7bcb67852b460
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-04 10:30:05 +0000

    Create 5-typed_arrays.js

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+  // create the buffer with length
+  const buffer = new ArrayBuffer(length);
+  const int8view = new DataView(buffer, 0);
+  int8view.setInt8(position, value);
+  return int8view;
+}
