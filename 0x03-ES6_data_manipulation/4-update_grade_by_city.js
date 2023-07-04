782f728d2bc6b2286023cde813a308dc14c46f3a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-04 06:44:22 +0000

    Add comments

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-param-reassign */
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  // filter students by the city
   const cityStudents = studentsList.filter((student) => student.location === city);
 
   const finalArray = cityStudents.map((student) => {
+    // check if student's grade is in list of grade or assign 'N/A'
     const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
 
     if (studentGrade.length !== 0) {
