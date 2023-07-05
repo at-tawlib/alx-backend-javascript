9997857f7625821ec99558bdf19888f8a02a4b47
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-05 05:23:45 +0000

    Do check if position is not within range

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
   // create the buffer with length
   const buffer = new ArrayBuffer(length);
   const int8view = new DataView(buffer, 0);
