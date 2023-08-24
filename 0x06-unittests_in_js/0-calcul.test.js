47e1281f0792292145e03618953254b19800929a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-24 03:27:08 +0000

    Add more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,13 @@ var assert = require('assert');
 
 describe('calculateNumber', () => {
     it('Rounds of a number', () => {
+        assert.equal(Math.round(1.3), 1);
+        assert.equal(Math.round(1.5), 2);
+        assert.equal(Math.round(1.7), 2);
+        assert.equal(Math.round(1), 1);
+    })
+
+    it('Sum of a numbers', () => {
         assert.equal(calculateNumber(1, 3), 4)
         assert.equal(calculateNumber(1, 3.7), 5)
         assert.equal(calculateNumber(1.2, 3.7), 5)
