33ed221b1910b02c20fc288da3a0e1ed329ccce3
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-24 04:00:15 +0000

    Recode file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,17 +2,23 @@ const calculateNumber = require('./0-calcul.js');
 var assert = require('assert');
 
 describe('calculateNumber', () => {
-    it('Rounds of a number', () => {
-        assert.equal(Math.round(1.3), 1);
-        assert.equal(Math.round(1.5), 2);
-        assert.equal(Math.round(1.7), 2);
-        assert.equal(Math.round(1), 1);
-    })
-
-    it('Sum of a numbers', () => {
+    it('calculateNumber', () => {
         assert.equal(calculateNumber(1, 3), 4)
+    });
+
+    it('calculateNumber', () => {
         assert.equal(calculateNumber(1, 3.7), 5)
+    });
+
+    it('calculateNumber', () => {
         assert.equal(calculateNumber(1.2, 3.7), 5)
+    });
+
+    it('calculateNumber', () => {
+        assert.equal(calculateNumber(1.2, 3.7), 5)
+    });
+
+    it('calculateNumber', () => {
         assert.equal(calculateNumber(1.5, 3.7), 6)
     });
 })
